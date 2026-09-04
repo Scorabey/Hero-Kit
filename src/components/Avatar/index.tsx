d591c9dbbacf597e62fe3610e9a19d0d45dca0ee
Scorabeyko <satlovdanijl@gmail.com>
2026-09-04 20:12:56 +0300

    Update Avatar component, add extend props

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,7 +1,8 @@
 import {AvatarWrapper} from "./styles.ts";
 import {AvatarIcon} from "../../icons/Avatar/Avatar.tsx";
+import type {HTMLAttributes} from "react";
 
-export interface AvatarProps {
+export interface AvatarProps extends HTMLAttributes<HTMLDivElement> {
     size?: "sm" | "md" | "lg";
     radius?: "none" | "sm" | "md" | "lg" | "full";
     color?: "primary" | "secondary" | "success" | "warning" | "danger" | "default";
@@ -10,10 +11,10 @@ export interface AvatarProps {
     src?: string;
 }
 
-export const Avatar = ({ size, src, color, radius, variant, isBordered  }: AvatarProps) => {
+export const Avatar = ({ size, src, color, radius, variant, isBordered, ...rest  }: AvatarProps) => {
 
     return (
-        <AvatarWrapper size={size} color={color} radius={radius} variant={variant} isBordered={isBordered}>
+        <AvatarWrapper size={size} color={color} radius={radius} variant={variant} isBordered={isBordered} {...rest}>
             {
                 variant === "icon"
                 ? <AvatarIcon />
